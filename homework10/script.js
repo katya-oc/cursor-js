84488905855a3c02dfb821f050a22c7a142d1c15
Katya Bielikova <katttya.oc@gmail.com>
2020-10-01 12:40:26 +0300

    added hw10

--- homework10/script.js
+++ homework10/script.js
@@ -1,22 +1,32 @@
  function playSound(e) {
-   const audio = document.querySelector(`audio[data-key='${e.key}']`);
-   const whiteKeys = document.querySelector(`.white[data-key='${e.key}']`);
-   const blackKeys = document.querySelector(`.black[data-key='${e.key}']`);
+   const audio = document.querySelector(`audio[data-key='${e.which}']`);
+   const key = document.querySelector(`.keys[data-key='${e.which}']`);
+
    if (!audio) return;
    audio.currentTime = 0; //при натисканні грає з початку
-   // audio.play();
+   audio.play();
 
-   if (whiteKeys) {
-     whiteKeys.classList.add('playing');
-     // return console.log(whiteKeys.classList);
-   } else if (blackKeys) {
-     blackKeys.classList.add('playingblack');
-     // return console.log(blackKeys.classList);
-   };
+   key.classList.add('playing');
  }
 
+ function removePlaying(e) {
+   if (e.propertyName !== 'box-shadow') return;
+   this.classList.remove('playing');
+ }
+
+ const keys = document.querySelectorAll('.keys');
+ keys.forEach(key => key.addEventListener('transitionend', removePlaying));
 
  window.addEventListener('keydown', playSound);
 
- //  const whites = document.querySelectorAll('.white');
- //  console.log(whites);
+ function mouseClick(e) {
+   const mouseaudio = document.querySelector(`audio[data-key='${e.target.dataset.key}']`);
+   const key = document.querySelector(`.keys[data-key='${e.target.dataset.key}']`);
+
+   if (!mouseaudio) return;
+   mouseaudio.currentTime = 0;
+   mouseaudio.play();
+
+ }
+
+ window.addEventListener('mousedown', mouseClick);
